e9036fad574df437fba74007bf3c598dc3c4b4b6
Wei-Ren Xue <weirenxue@gmail.com>
2023-02-03 15:22:02 +0800

    add reflective visitor

--- behavioral/visitor/reflective_visitor/main.go
+++ behavioral/visitor/reflective_visitor/main.go
@@ -6,27 +6,26 @@ import (
 )
 
 type Expression interface {
-	Print(sb *strings.Builder)
 }
 
 type DoubleExpression struct {
 	value float64
 }
 
-func (d *DoubleExpression) Print(sb *strings.Builder) {
-	sb.WriteString(fmt.Sprintf("%g", d.value))
-}
-
 type AdditionExpression struct {
 	left, right Expression
 }
 
-func (a *AdditionExpression) Print(sb *strings.Builder) {
-	sb.WriteString("(")
-	a.left.Print(sb)
-	sb.WriteString("+")
-	a.right.Print(sb)
-	sb.WriteString(")")
+func Print(e Expression, sb *strings.Builder) {
+	if de, ok := e.(*DoubleExpression); ok {
+		sb.WriteString(fmt.Sprintf("%g", de.value))
+	} else if ae, ok := e.(*AdditionExpression); ok {
+		sb.WriteString("(")
+		Print(ae.left, sb)
+		sb.WriteString("+")
+		Print(ae.right, sb)
+		sb.WriteString(")")
+	}
 }
 
 func main() {
@@ -39,6 +38,6 @@ func main() {
 	}
 
 	sb := strings.Builder{}
-	e.Print(&sb)
+	Print(e, &sb)
 	fmt.Println(sb.String())
 }
