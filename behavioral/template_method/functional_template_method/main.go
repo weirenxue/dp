74dc210340cede5ab3cb6816dc23ad9b33f1bd1d
Wei-Ren Xue <weirenxue@gmail.com>
2023-02-03 14:40:53 +0800

    add functional template method

---
+++ behavioral/template_method/functional_template_method/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func PlayGame(start, takeTurn func(), haveWinner func() bool, winningPlayer func() int) {
+	start()
+	for !haveWinner() {
+		takeTurn()
+	}
+	fmt.Printf("Player %d wins.\n", winningPlayer())
+}
+
+func main() {
+	turn, maxTurns, currentPlayer := 0, 10, 0
+
+	start := func() {
+		fmt.Println("Starting a game of chess")
+	}
+
+	takeTurn := func() {
+		turn++
+		fmt.Printf("Turn %d taken by player %d\n", turn, currentPlayer)
+		currentPlayer = 1 - currentPlayer
+	}
+
+	haveWinner := func() bool {
+		return turn >= maxTurns
+	}
+
+	winningPlayer := func() int {
+		return currentPlayer
+	}
+
+	PlayGame(start, takeTurn, haveWinner, winningPlayer)
+}
